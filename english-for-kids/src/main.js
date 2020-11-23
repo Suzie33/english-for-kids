b8cd9ec1d939dcf5020ac2f4426338c0cf441078
Valentina Ponamareva <valiazin@gmail.com>
2020-11-23 21:01:10 +0300

    feat: add menu

--- english-for-kids/src/main.js
+++ english-for-kids/src/main.js
@@ -2,3 +2,14 @@ import './css/main.scss';
 
 import './scripts/cards';
 import './scripts/category';
+
+// menu-btn
+
+const menuIcon = document.querySelector('.menu-icon__toggle');
+const navigation = document.querySelector('#nav');
+
+menuIcon.addEventListener('click', () => {
+  navigation.classList.toggle('nav--visible');
+  menuIcon.classList.toggle('menu-icon__toggle--open');
+  document.body.classList.toggle('noScroll');
+});

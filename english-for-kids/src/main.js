7a2c01684d1847371937e610aa1b1f2d34b81292
Valentina Ponamareva <valiazin@gmail.com>
2020-11-24 13:33:05 +0300

    feat: add overlay

--- english-for-kids/src/main.js
+++ english-for-kids/src/main.js
@@ -7,9 +7,11 @@ import './scripts/category';
 
 const menuIcon = document.querySelector('.menu-icon__toggle');
 const navigation = document.querySelector('#nav');
+const overlay = document.querySelector('.overlay');
 
 menuIcon.addEventListener('click', () => {
   navigation.classList.toggle('nav--visible');
   menuIcon.classList.toggle('menu-icon__toggle--open');
+  overlay.classList.toggle('overlay--visible');
   document.body.classList.toggle('noScroll');
 });
